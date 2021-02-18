109cc067dc2337b22112bfe3ba81a0c5f5e5d4dd
EdgarBryanLM <55665769+EdgarBryanLM@users.noreply.github.com>
2021-02-17 23:46:36 -0600

    Cambios

---
+++ ProyectoWebII/Daos/DaoUsuario.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using MySql.Data.MySqlClient;
+using ProyectoWebII.Pojos;
+
+namespace ProyectoWebII
+{
+    public class Class1
+    {
+
+        MySqlConnection conex = new MySqlConnection();
+        public void conexion()
+        {
+            try
+            {
+                conex.ConnectionString = "server=localhost; database=pagina; user=root; pwd=root; port=3305";
+
+                
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+
+        }
+
+        public bool addUsuario(ref ObjUsuarios objUsuarios)
+        {
+            MySqlConnection conexxion = new MySqlConnection();
+            MySqlCommand comando = new MySqlCommand();
+
+
+
+            conexxion.ConnectionString = "server=localhost; database=libros; user=root; pwd=root; port=3305";
+            conexxion.Open();
+
+            /// AGREGAR EL REGISTRO A LA BASE DE DATOS
+            string strSQL = "Insert into login (correo,pass,nombre,apellidos,telefono) values('" + objUsuarios. + "','" + objLibros.Titulo + "', " + objLibros.NumeroEdicion + ", " + objLibros.Anio + ",'" + objLibros.NombresAutores + "','" + objLibros.PaisPublicacion + "','" + objLibros.Sinopsis + "','" + objLibros.Carrera + "','" + objLibros.Materia + "');";
+            comando = new MySqlCommand(strSQL, conexxion);
+            // ("132-7462-4757-934","Maria DB",1,2008,"Giovanni Tapia","Mexico","Descubre el funcionmiento de Maria DB","Sistemas computacionales","Taller de base de datos");
+
+
+            comando.ExecuteNonQuery();
+            comando.Dispose();
+            conexxion.Close();
+            return true;
+
+            /// FINALIZAMOS LA CONEXION CERRAMOS TODO
+
+        }
+
+    }
+}
---
+++ ProyectoWebII/Pojos/ObjUsuarios.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ProyectoWebII.Pojos
+{
+    public class ObjUsuarios
+    {
+
+
+        public int idUsuario { get; set; }
+        public string correo { get; set; }
+        public string pass { get; set; }
+        public string nombre { get; set; }
+        public string apellidos { get; set; }
+        public int telefono { get; set; }
+        
+       
+
+        public ObjUsuarios(string correo, string pass, string nombre, string apellidos,int telefono)
+        {
+            this.correo = correo;
+            this.pass = pass;
+            this.nombre = nombre;
+            this.apellidos = apellidos;
+            this.telefono = telefono;
+
+        }
+    }
+}
