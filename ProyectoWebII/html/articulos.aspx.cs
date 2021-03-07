f70501aca4c1c1533714ee34d78a5e736fd5d80b
Jesus Leyva <chucho.leyva91@gmail.com>
2021-03-07 12:40:52 -0600

    Prueba de js en C#

--- ProyectoWebII/html/articulos.aspx.cs
+++ ProyectoWebII/html/articulos.aspx.cs
@@ -24,35 +24,9 @@ namespace ProyectoWebII.html
             XmlNodeList listaArticulos = document.SelectNodes("Articulos/Articulo");
             foreach (XmlNode item in listaArticulos)
             {
-                HtmlGenericControl articuloHtml = new HtmlGenericControl("article");
-                if (item.SelectSingleNode("Articulos").SelectSingleNode("IdCotizacion").InnerText == TextBox1.Text)
-                {
-                    item.ChildNodes[0].ChildNodes[1].InnerText = TextBox2.Text;
-                    item.ChildNodes[0].ChildNodes[2].InnerText = TextBox3.Text;
-                    item.ChildNodes[1].ChildNodes[0].InnerText = TextBox4.Text;
-                    item.ChildNodes[1].ChildNodes[1].InnerText = TextBox5.Text;
-                    item.ChildNodes[1].ChildNodes[2].InnerText = TextBox6.Text;
-                    item.ChildNodes[2].ChildNodes[0].InnerText = TextBox7.Text;
-                    item.ChildNodes[2].ChildNodes[1].InnerText = TextBox8.Text;
-                    item.ChildNodes[2].ChildNodes[2].InnerText = TextBox9.Text;
-                    item.ChildNodes[2].ChildNodes[3].InnerText = TextBox10.Text;
-                    doc.Save(path);
-                    break;
-                }
+                string javaScript = "MostrarMensaje();";
+                ScriptManager.RegisterClientScriptBlock(this, this.GetType(), "script", javaScript, true);
             }
-
-            //< article >
-            //        < a title = "Smach" href = "articulo.aspx" >< img src = "../img/7.jpg" ></ a >
-
-            //               < div >
-
-            //                   < a class="article_titulo" href="articulo.php">
-            //                #ViernesRetro: El Smash anda barato
-            //            </a>
-            //            <p class="article_texto">El Smach anda barato, cómpralo en corto.</p>
-            //            <p class="fecha">31/10/2020</p>
-            //        </div>
-            //    </article>
         }
     }
 }
